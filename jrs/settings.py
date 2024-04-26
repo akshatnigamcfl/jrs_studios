8a72fd877470ad32c0892f00812d79a69906d0fa
Akshat Nigam <akshatnigamcfl@gmail.com>
2024-04-26 11:18:57 +0530

    v2

--- jrs/settings.py
+++ jrs/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/opt/render/project/src/staticfiles'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 if not DEBUG:
